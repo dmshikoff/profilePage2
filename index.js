77f092c30670a55e5f4e922b44c30fb2620ae07d
Daniel Shikoff <danielshikoff@Daniels-MacBook-Pro.local>
2018-03-06 21:51:50 -0800

    first project tile completed

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ let textCont = document.querySelector(".project-tile-header")
 
 for(let i of contCol){
   i.addEventListener("mouseover", function (event) {
-    this.children[1].classList.add("imgBoxIn")
+    this.children[2].classList.add("imgBoxIn")
     this.children[0].classList.add("project-tile-header-in")
+    this.children[3].classList.add("octoCatIn")
+    this.children[1].classList.add("deployedLinkIn")
   })
   i.addEventListener("mouseout", function (event) {
-    this.children[1].classList.remove("imgBoxIn")
+    this.children[2].classList.remove("imgBoxIn")
     this.children[0].classList.remove("project-tile-header-in")
+    this.children[3].classList.remove("octoCatIn")
+    this.children[1].classList.remove("deployedLinkIn")
   })
 }
