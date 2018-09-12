db868e08d967f942e3933bfbacd6b01711f580cb
dmshikoff <dmshikoff@gmail.com>
2018-09-12 15:09:29 -0700

    transitioned navbar

--- index.js
+++ index.js
@@ -16,3 +16,26 @@ for(let i of contCol){
     this.children[1].classList.remove("deployedLinkIn")
   })
 }
+
+$(window).scroll(function() {
+  // 100 = The point you would like to fade the nav in.
+    
+    if ($(window).scrollTop() > 400 ){
+      
+       $('.bg').addClass('show');
+      
+    } else {
+      
+      $('.bg').removeClass('show');
+      
+     };   	
+  });
+  
+  $('.scroll').on('click', function(e){		
+      e.preventDefault()
+      
+    $('html, body').animate({
+        scrollTop : $(this.hash).offset().top
+      }, 1500);
+  });
+  
